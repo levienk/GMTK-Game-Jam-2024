4d5eeb77f2707ec723edbcbdecdca433890c0dd8
Nate Fodge <138825290+euclideanlogic@users.noreply.github.com>
2024-08-17 14:47:48 -0700

    Fixed resolution issues
    Fixed scaling, allowed the full 16:9 resolution.

--- game.gd
+++ game.gd
@@ -1,18 +1,11 @@
 extends Node2D
-
+# Dimensions: Lucas art 112*112 480 * 360
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# These are tests that can be deleted later
 	#print(WordDictionary.contains("health"))
 	#print(WordDictionary.contains("healtt"))
-	
-	# These are tests for the TextCard, and can be deleted later.
-	
-	$WordCard.set_text("This")
-	
-	$WordCard2.set_text("works")
-	
-	$WordCard3.set_text("well.")
+	pass
 	
 	
 	
