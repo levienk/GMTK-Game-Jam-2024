183ccbe691a70833b368215ca34f02d2e9b27c67
Kade Levien <kade.levien@gmail.com>
2024-08-17 11:06:47 -0700

    Update monkey chance system
    TODO: implement word generation for words in the current quote

--- monkey.gd
+++ monkey.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-@export var random_time = 0.5
-@export var valid_time = 3.0
-@export var valid_word_chance = 0.1
-@export var max_word_length = 10
+static var random_time = 0.5
+static var valid_time = 3.0
+static var valid_word_chance = 0.1
+# Must be less than valid_word_chance
+static var valid_word_in_quote_chance = 0.05
+static var max_word_length = 10
 
-var possible_chars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
+const possible_chars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
 						"o","p","q","r","s","t","u","v","w","x","y","z"]
 var random_word: String
 
@@ -15,14 +17,19 @@ var random_word: String
 func _on_timer_timeout() -> void:
 	timer.stop()
 	random_word = ""
+	var rand = randf()
+	
 	# Generate random word
-	if (randf() > valid_word_chance):
+	if (rand < valid_word_in_quote_chance):
+		# TODO The word is generated from the quote
+		random_word = WordDictionary.get_random()
+	elif (rand < valid_word_chance):
+		# The word is generated from the dictionary
+		random_word = WordDictionary.get_random()
+	else:
 		# The word is randomly generated
 		for i in range(randi() % max_word_length + 1):
 			random_word += possible_chars.pick_random()
-	else:
-		# The word is generated from the dictionary
-		random_word = WordDictionary.get_random()
 	
 	if (WordDictionary.contains(random_word)):
 		timer.wait_time = valid_time
