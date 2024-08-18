626d2b999bb03cacf7210dc2004679fc0f6bb3cb
Kade Levien <kade.levien@gmail.com>
2024-08-17 19:08:00 -0700

    Create WordBox

---
+++ interface/text_boundary.gd
@@ -0,0 +1,12 @@
+extends TextureRect
+
+const max_length = 140
+
+func _ready() -> void:
+	if $Label.size.x > max_length:
+		$Label.clip_text = true
+		$Label.size.x = max_length
+		
+	size = $Label.size
+	size.x += 16
+	
