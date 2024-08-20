519ddcd336d7bb29ff7499f030b38be34e8e3af3
Kade Levien <kade.levien@gmail.com>
2024-08-20 02:32:20 -0700

    Hide WordBox while shop is open

--- word_bank.gd
+++ word_bank.gd
@@ -3,6 +3,11 @@ extends Node2D
 @onready var WordBox = preload("res://interface/word_box.tscn")
 @onready var sfx = $CreateSFX
 
+var is_hidden = false
+
+func _ready() -> void:
+	SignalBus.shop_clicked.connect(_on_shop_clicked)
+
 func _on_line_edit_new_word_typed(word: String) -> void:
 	
 	sfx.play()
@@ -11,3 +16,11 @@ func _on_line_edit_new_word_typed(word: String) -> void:
 	$ScrollContainer/HFlowContainer.add_child(new_word_container)
 	$ScrollContainer/HFlowContainer.sort_children()
 	
+# Hide the WordBoc so it doesn't cover the shop
+func _on_shop_clicked():
+	if is_hidden:
+		self.show()
+	else:
+		self.hide()
+		
+	is_hidden = !is_hidden
